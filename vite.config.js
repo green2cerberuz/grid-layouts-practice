5e2de9c4d1e92502b37566825b166946d1bad859
cristhian <green2cerberuz@gmail.com>
2024-03-02 08:05:58 -0500

    feat: add music keyboard pattern basic

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         pattern1: resolve(__dirname, 'design/pattern1/index.html'),
         pattern2: resolve(__dirname, 'design/pattern2/index.html'),
+        pattern3: resolve(__dirname, 'design/pattern3/index.html'),
       },
     },
   },
