207d38e3145ad823b04d7ed108ecfe2d38713f96
green2cerberuz <67993031+green2cerberuz@users.noreply.github.com>
2024-03-07 06:51:36 -0500

    feat: add dynamic importer to improve the implementation of new grid prototypes (#8)

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,32 @@
 // vite.config.js
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
+const EXCLUDE_DIRECTORIES = ['node_modules', 'dist', 'public'];
+
+const getProjectFiles = (dir, basePrefix = '') => {
+  const files = fs.readdirSync(dir, { withFileTypes: true });
+  const input = {};
+
+  for (const file of files) {
+    const resPath = path.resolve(dir, file.name);
+    if (file.isDirectory() && !EXCLUDE_DIRECTORIES.includes(file.name)) {
+      const nested = getProjectFiles(resPath, file.name);
+      Object.assign(input, nested); // does a shallow copy of nested into input
+    } else if (file.name.endsWith('.html')) {
+      const key = `${basePrefix}`;
+      input[key] = resPath;
+    }
+  }
+  return input;
+};
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        pattern1: resolve(__dirname, 'design/pattern1/index.html'),
-        pattern2: resolve(__dirname, 'design/pattern2/index.html'),
-        pattern3: resolve(__dirname, 'design/pattern3/index.html'),
-        pattern4: resolve(__dirname, 'design/pattern4/index.html'),
-        pattern5: resolve(__dirname, 'design/pattern5/index.html'),
-        pattern6: resolve(__dirname, 'design/pattern6/index.html'),
-      },
+      input: getProjectFiles(__dirname, 'designs'),
     },
   },
 });
